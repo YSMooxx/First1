4e13d48e60d00f9a0f5a119381310910326730f7
yx <584025430@qq.com>
2022-05-05 15:10:03 +0800

    1

--- First1/ViewController.swift
+++ First1/ViewController.swift
@@ -6,12 +6,14 @@
 //
 
 import UIKit
+import Darwin
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let myString = "Hello, World!"
+        print(myString)
     }
 
 
