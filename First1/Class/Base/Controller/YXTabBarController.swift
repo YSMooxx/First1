b43fcda71e0b810bf463a3f8370f7375039fc591
yx <584025430@qq.com>
2022-05-25 17:23:28 +0800

    tabbar

--- First1/Class/Base/Controller/YXTabBarController.swift
+++ First1/Class/Base/Controller/YXTabBarController.swift
@@ -7,20 +7,44 @@
 
 import UIKit
 
-
 class YXTabBarContrller: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupUI()
+        AddChileVCWithArray(array: VCArray)
+        
+//        let dic = ["title":"123","name":"ddd"]
+//
+//        print(dic)
         
     }
     
     func setupUI() {
         
         
+        
     }
     
-    var model = ControllerModel()
+    var VCArray : NSArray = [["YClass":"FirstController","YTitle":"消息","YImage":"","YSelImage":"",],["YClass":"DelegateController","YTitle":"代理","YImage":"","YSelImage":"",]]
+    
+    func AddChileVCWithArray(array:NSArray) {
+        
+        for s in array {
+            
+            AddChileVCWithDic(dic: s as! Dictionary<String, String>)
+        }
+        
+    }
+    
+    func AddChileVCWithDic(dic:Dictionary<String,String>) {
+        
+        let YClass:String = dic["YClass"] ?? ""
+        
+        print(YClass)
+        
+    }
     
     
 }
