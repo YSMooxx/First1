0bd6d5a57342684f05ae876ab83e0be6257a75cb
yx <584025430@qq.com>
2022-07-13 10:28:48 +0800

    tabbar

--- First1/Class/Base/Controller/YXTabBarController.swift
+++ First1/Class/Base/Controller/YXTabBarController.swift
@@ -24,7 +24,7 @@ class YXTabBarContrller: UITabBarController {
     
     func AddChileVCWithArray() {
         
-        let VCArray : NSArray = [["YClass":"FirstController","YTitle":"消息","YImage":"","YSelImage":"",],["YClass":"DelegateController","YTitle":"代理","YImage":"","YSelImage":"",]]
+        let VCArray : NSArray = [["YClass":"FirstController","YTitle":"消息","YImage":"","YSelImage":""],["YClass":"DelegateController","YTitle":"代理","YImage":"","YSelImage":""],["YClass":"ViewController","YTitle":"我的","YImage":"","YSelImage":""]]
         
         for controller in VCArray {
             
@@ -36,10 +36,17 @@ class YXTabBarContrller: UITabBarController {
     func AddChileVCWithDic(dic:Dictionary<String,String>) {
         
         let YClass:String = dic["YClass"] ?? ""
-        
+        let YTitle:String = dic["YTitle"] ?? ""
+        let YImage:String = dic["YImage"] ?? ""
+        print(YTitle)
+        print(YImage)
         let namespace = Bundle.main.infoDictionary!["CFBundleExecutable"]as!String
         let vcClass = NSClassFromString(namespace+"."+YClass)!as!UIViewController.Type
         let navigationVC = UINavigationController(rootViewController: vcClass.init())
+        addChildViewController(navigationVC)
+        
+        navigationVC.title = YTitle
+        
         addChildViewController(navigationVC)
         
     }
