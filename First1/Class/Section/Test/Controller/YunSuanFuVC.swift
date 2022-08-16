4cf69c7eaa9ec070f362dd79a0962c7e9b0e88b9
yx <584025430@qq.com>
2022-08-16 21:03:44 +0800

    运算符1

--- First1/Class/Section/Test/Controller/YunSuanFuVC.swift
+++ First1/Class/Section/Test/Controller/YunSuanFuVC.swift
@@ -11,7 +11,7 @@ class YunSuanFuVC:UIViewController {
     
     override func viewDidLoad() {
         
-        test1()
+        test4()
     }
     
     //算数符
@@ -28,5 +28,44 @@ class YunSuanFuVC:UIViewController {
         print("A += 1 后 A 的值为：\(A)")
         B -= 1   // 类似 B--
         print("B -= 1 后 B 的值为：\(B)")
+    }
+    
+    //比较运算符
+    func test2() {
+        
+        let A = 10
+        let B = 20
+
+        print("A == B 结果为：\(A == B)")
+        print("A != B 结果为：\(A != B)")
+        print("A > B 结果为：\(A > B)")
+        print("A < B 结果为：\(A < B)")
+        print("A >= B 结果为：\(A >= B)")
+        print("A <= B 结果为：\(A <= B)")
+
+    }
+    
+    //位运算符
+    func test3() {
+        
+        let A = 60  // 二进制为 0011 1100
+        let B = 13 // 二进制为 0000 1101
+
+        print("A&B 结果为：\(A&B)")
+        print("A|B 结果为：\(A|B)")
+        print("A^B 结果为：\(A^B)")
+        print("~A 结果为：\(~A)")
+    }
+    
+    //逻辑运算符
+    func test4() {
+        
+        let A = true
+        let B = false
+
+        print("A && B 结果为：\(A && B)")
+        print("A || B 结果为：\(A || B)")
+        print("!A 结果为：\(!A)")
+        print("!B 结果为：\(!B)")
     }
 }
